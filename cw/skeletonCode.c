41c1021f9334748ddddf9be4c23287e9a7fe8f0a
ASethi9876 <146444827+ASethi9876@users.noreply.github.com>
2024-02-13 18:53:08 +0000

    commit

---
+++ cw/skeletonCode.c
@@ -0,0 +1,24 @@
+struct maze{
+    int height;
+    int length;
+    char* mazeArray[height][length];
+}
+
+int moveUp{}
+int moveLeft{}
+int moveDown{}
+int moveRight{}
+int checkTile{}
+void showMap{}
+
+int main{
+    //load from file
+    //loop until E reached:
+        //ask for input
+        //w - call moveUp
+        //a - call moveLeft
+        //s - call moveDown
+        //d - call moveRight
+        //m - call showMap
+    //if end found give win message
+}
