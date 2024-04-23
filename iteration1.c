8af14674b78e6d77da9b05d1cf8fdfe721d22c8d
ASethi9876 <146444827+ASethi9876@users.noreply.github.com>
2024-04-23 20:18:27 +0000

    commit

--- iteration1.c
+++ iteration1.c
@@ -23,12 +23,14 @@ int main(int argc, char* argv[]){
     int height = atoi(argv[2]), width = atoi(argv[3]);
 
     char maze[height][width];
+    int visitedMap[height][width];
     for (int i = 0; i < height; i++){
         for (int j = 0; j < width; j++){
             maze[i][j] = '#';
+            visitedMap[i][j] = 0;
         }
     }
-    int visited[height * width][2];
+    int visited[height*width][2];
     srand(time(0));
     int y = 0, x = 0;
     y = randInt(0, height - 1);
@@ -38,28 +40,27 @@ int main(int argc, char* argv[]){
     bool moved,searchN,searchE,searchS,searchW,alternatives,noMove;
     while (count >= 0) {
         moved = false;
-        if (y == 0){
+        if (y == 0 || (y > 1 && maze[y-2][x] == ' ')){
             searchN = false;
         } else {
             searchN = true;
         }
-        if (x == 0){
+        if (x == 0 || (x > 1 && maze[y][x-2] == ' ')){
             searchW = false;
         } else {
             searchW = true;
         }
-        if (y == (height - 1)){
+        if (y == (height - 1) || (y < (height - 2) && maze[y+2][x] == ' ')){
             searchS = false;
         } else {
             searchS = true;
         }
-        if (x == (width - 1)){
+        if (x == (width - 1) ||  (x < (width - 2) && maze[y][x+2] == ' ')){
             searchE = false;
         } else {
             searchE = true;
         }
         while (moved == false){ // Repeat until new valid coordinate found
-        printf("%d\n",count);
             direction = randInt(0,3);
             if (direction == 0 && searchN == true){ // N
                 y -= 1;
@@ -79,34 +80,46 @@ int main(int argc, char* argv[]){
             } else {
                 alternatives = true;
             }
-
+    
             if (maze[y][x] == '#'){
-                maze[y][x] = 'M';
+                maze[y][x] = ' ';
                 if (alternatives = true){ // Will only go back if other options
-                    visited[count][0] = y, visited[count][1] = x;
+                    visited[count][0] = y, visited[count][1] = x, visitedMap[y][x] = 1;
                     count += 1;
                 }
                 moved = true;
 
-                // for (int a = 0; a < height; a++){
-                //     for (int b = 0; b < width; b++){
-                //         char e = maze[a][b];
-                //         printf("%c", e);
-                //     }
-                //     printf("\n");
-                // }
-                // printf("\n");
-                maze[y][x] = ' ';
-            } else if (alternatives == false){ // If no valid options left
+            } else if (alternatives == false || visitedMap[y][x] == 1){ // Returns to most recent node with alternative routes if no alternatives here or its been here before
                 count -= 1;
+                printf("%d\n",count);
                 if (count >= 0){
                     y = visited[count][0], x = visited[count][1]; // Go back to most recent
                 }
                 moved = true;
             }
+            for (int a = 0; a < height; a++){
+                for (int b = 0; b < width; b++){
+                    if (a == y && b == x){
+                        printf("m");
+                    } else {
+                        printf("%d", visitedMap[a][b]);
+                    }
+                }
+                printf("\n");
+            }
+            printf("\n");
         }
     }
-    maze[y][x] = 'E'; // End of maze
+    bool endChosen = false;
+    while (endChosen == false){
+        y = randInt(0,height);
+        x = randInt(0,width);
+        if (maze[y][x] == ' '){
+            maze[y][x] = 'E';
+            endChosen = true;
+            printf("%d %d %c \n",y,x,maze[y][x]);
+        }
+    }
     for (int y = 0; y < height; y++){
         for (int x = 0; x < width; x++){
             char e = maze[y][x];
