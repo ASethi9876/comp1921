5703afea7281082d92897efc0075599a3669fa13
ASethi9876 <146444827+ASethi9876@users.noreply.github.com>
2024-04-30 19:16:26 +0000

    commit

--- mazegame/maze.c
+++ mazegame/maze.c
@@ -322,7 +322,7 @@ int main(int argc, char* argv[]){
         getInput(this);
         printf("You have completed the maze!\n");
         freeMaze(this);
-        //exit(EXIT_SUCCESS);
+        exit(EXIT_SUCCESS);
     }
     return 0;
 }
